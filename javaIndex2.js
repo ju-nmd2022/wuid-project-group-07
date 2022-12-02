8c8208e9debdbfb8ccceee66c5325626cbbd8f31
AmelaMusic <muam21gk@student.ju.se>
2022-12-02 12:10:09 +0100

    changes

--- javaIndex2.js
+++ javaIndex2.js
@@ -1,8 +1,15 @@
 function showOverlay() {
-    document.getElementById("overlay").style.display = "block";
-   }
-    
-   function hideOverlay() {
-    document.getElementById("overlay").style.display = "none";
-   }
-   
+  document.getElementById("overlay").style.display = "block";
+}
+
+function hideOverlay() {
+  document.getElementById("overlay").style.display = "none";
+}
+
+function openNav() {
+  document.getElementById("side-navigation").style.width = "414px";
+}
+
+function closeNav() {
+  document.getElementById("side-navigation").style.width = "0";
+}
